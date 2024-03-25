f47c8f3dd564a8b60f995d7960cbda0c3e695e5b
Eduard Zelenay <xzelenay@stuba.sk>
2024-03-25 16:10:09 +0100

    fixed proximity warnings

--- demoRMR-all/demoRMR/myframe.cpp
+++ demoRMR-all/demoRMR/myframe.cpp
@@ -130,236 +130,170 @@ void MyFrame::paintEvent(QPaintEvent *event) {
                 if(main_window->updateLaserPicture==1) ///ak mam nove data z lidaru
                 {
                     main_window->updateLaserPicture=0;
-
-                    //painter.setPen(pero);
-                    //teraz tu kreslime random udaje... vykreslite to co treba... t.j. data z lidaru
-                    //   std::cout<<copyOfLaserData.numberOfScans<<std::endl;
-                    // for(int k=0;k<main_window->copyOfLaserData.numberOfScans/*360*/;k++)
-                    // {
-                    //     int dist=main_window->copyOfLaserData.Data[k].scanDistance/20; ///vzdialenost nahodne predelena 20 aby to nejako vyzeralo v okne.. zmen podla uvazenia
-                    //     int xp=rect.width()-(rect.width()/2+dist*2*sin((360.0-main_window->copyOfLaserData.Data[k].scanAngle)*3.14159/180.0))+rect.topLeft().x(); //prepocet do obrazovky
-                    //     int yp=rect.height()-(rect.height()/2+dist*2*cos((360.0-main_window->copyOfLaserData.Data[k].scanAngle)*3.14159/180.0))+rect.topLeft().y();//prepocet do obrazovky
-                    //     if(rect.contains(xp,yp))//ak je bod vo vnutri nasho obdlznika tak iba vtedy budem chciet kreslit
-                    //         painter.drawEllipse(QPoint(xp, yp),2,2);
-                    // }
                 }
+
+            double minUpDistance = 100000;
+            double minDownDistance = 100000;
+            double minLeftDistance = 100000;
+            double minRightDistance = 100000;
+
+            for(int k=0; k < main_window->copyOfLaserData.numberOfScans/*360*/;k++){
+                    double dist=main_window->copyOfLaserData.Data[k].scanDistance;
+                    double uhol = 360.0-main_window->copyOfLaserData.Data[k].scanAngle;
+                    if (dist!=0.0){
+                        if(uhol < 45 || uhol >315 ){
+                            //top
+                            if(dist < minUpDistance){
+                                minUpDistance = dist;
+                            }
+                        }
+                        if(uhol <= 135 && uhol >= 45){
+                            //left
+                            if(dist < minLeftDistance){
+                                minLeftDistance = dist;
+                            }
+                        }
+                        if(uhol < 225 && uhol > 135){
+                            //bottom
+                            if(dist < minDownDistance){
+                                minDownDistance = dist;
+                            }
+                        }
+                        if(uhol <= 315 && uhol >= 225){
+                            //right
+                            if(dist < minRightDistance){
+                                minRightDistance = dist;
+                            }
+                        }
+                    }
+                }
+
+                //top
+
+            if(minUpDistance < YELLOW_DISTANCE){
+                QPainter painter(this);
+                painter.setBrush(Qt::red);
+                painter.drawPolygon(topPolygon);
+            }
+            else if(minUpDistance < GREEN_DISTANCE){
+                QPainter painter(this);
+                painter.setBrush(Qt::yellow);
+                painter.drawPolygon(topPolygon);
+            }
+            else{
+                QPainter painter(this);
+                painter.setBrush(Qt::green);
+                painter.drawPolygon(topPolygon);
             }
 
+            //left
 
-            for(int k=0; k < main_window->copyOfLaserData.numberOfScans/*360*/;k++)
-            {
-                double dist=main_window->copyOfLaserData.Data[k].scanDistance;
-                double uhol = 360.0-main_window->copyOfLaserData.Data[k].scanAngle;
+            if(minLeftDistance < YELLOW_DISTANCE){
+                QPainter painter(this);
+                painter.setBrush(Qt::red);
+                painter.drawPolygon(leftPolygon);
+            }
+            else if(minLeftDistance < GREEN_DISTANCE){
+                QPainter painter(this);
+                painter.setBrush(Qt::yellow);
+                painter.drawPolygon(leftPolygon);
+            }
+            else{
+                QPainter painter(this);
+                painter.setBrush(Qt::green);
+                painter.drawPolygon(leftPolygon);
+            }
 
-                // // Convert the angle to radians
-                // double angleRad = qDegreesToRadians(uhol);
+            //bottom
 
-                // // Calculate the x and y coordinates based on distance and angle
-                // int xp = static_cast<int>(dist * qCos(angleRad)); // x coordinate
-                // int yp = static_cast<int>(dist * qSin(angleRad)); // y coordinate
+            if(minDownDistance < YELLOW_DISTANCE){
+                QPainter painter(this);
+                painter.setBrush(Qt::red);
+                painter.drawPolygon(bottomPolygon);
+            }
+            else if(minDownDistance < GREEN_DISTANCE){
+                QPainter painter(this);
+                painter.setBrush(Qt::yellow);
+                painter.drawPolygon(bottomPolygon);
+            }
+            else{
+                QPainter painter(this);
+                painter.setBrush(Qt::green);
+                painter.drawPolygon(bottomPolygon);
+            }
 
-                // // Calculate the position of the image based on the LIDAR data coordinates and frame dimensions
-                // int x = rect.center().x() + xp; // x position relative to the frame center
-                // int y = rect.center().y() + yp; // y position relative to the frame center
+            //right
 
-                // // Ensure that the image stays within the bounds of the frame
-                // x = qBound(rect.left(), x, rect.right() - 240);
-                // y = qBound(rect.top(), y, rect.bottom() - 240);
+            if(minRightDistance < YELLOW_DISTANCE){
+                QPainter painter(this);
+                painter.setBrush(Qt::red);
+                painter.drawPolygon(rightPolygon);
+            }
+            else if(minRightDistance < GREEN_DISTANCE){
+                QPainter painter(this);
+                painter.setBrush(Qt::yellow);
+                painter.drawPolygon(rightPolygon);
+            }
+            else{
+                QPainter painter(this);
+                painter.setBrush(Qt::green);
+                painter.drawPolygon(rightPolygon);
+            }
 
+            if(main_window->backup_assistant==true){
 
-                if(uhol < 45 || uhol >315 ){
+                // Calculate the size of the backup assistant rectangle (one-third of the width and height of the frame)
+                int assistantWidth = rect.width() / 3;
+                int assistantHeight = rect.height() / 3;
 
-                    double bx = dist*cos(uhol*3.14159/180.0);
-                    double by = dist*sin(uhol*3.14159/180.0);
+                // Calculate the position of the backup assistant rectangle at the top in the middle of the frame
+                int assistantX = rect.center().x() - assistantWidth / 2;
+                int assistantY = rect.top();
 
-                    //int x = rect.width()/2  - ( (681.743*by) / (bx-14.5) );
-                    //int y = rect.height()/2 + ( (681.743*(-21+11.5)) / (bx-14.5)  )/10;
-                    //painter.drawEllipse(QPoint(x, y),2,2);
+                // Define the backup assistant rectangle
+                QRect assistantRect(assistantX, assistantY, assistantWidth, assistantHeight);
+                // assistantRect.translate(0, rect.top()+50);
+                // Draw the backup assistant rectangle
+                QPainter painter(this);
+                painter.fillRect(assistantRect, Qt::black); // Fill the rectangle with blue color
 
-                    int x = rect.width() / 2 - (681.743 * by) / (bx - 14.5);
-                    int y = rect.height() / 2 + ((681.743 * (-21 + 11.5)) / (bx - 14.5)) / 10;
+                // Loop through the laser data and draw backup assistant points within the rectangle
+                for (int k = 0; k < main_window->copyOfLaserData.numberOfScans; k++) {
+                    int dist = main_window->copyOfLaserData.Data[k].scanDistance; // Distance divided by 20 (adjust as needed)
+                    double angle = 360.0 - main_window->copyOfLaserData.Data[k].scanAngle;
+                    int xp = rect.width() - (rect.width() / 2 + dist/5 * 2 * sin(angle * M_PI / 180.0)) + rect.topLeft().x();
+                    int yp = rect.height() - (rect.height() / 2 + dist/5 * 2 * cos(angle * M_PI / 180.0)) + rect.topLeft().y();
 
-                    // Calculate the position of the obstacle relative to the frame's top-left corner
-                    x -= image.width() / 2; // Adjust for the image width
-                    y -= image.height() / 2; // Adjust for the image height
+                    int assistantXp = assistantRect.x() + (xp - rect.x()) * assistantRect.width() / rect.width();
+                    int assistantYp = assistantRect.y() + (yp - rect.y()) * assistantRect.height() / rect.height();
 
-                    if(rect.contains(x,y))//ak je bod vo vnutri nasho obdlznika tak iba vtedy budem chciet kreslit
-                    {
-                        if(dist < YELLOW_DISTANCE){
-                            QPainter painter(this);
-                            painter.setBrush(Qt::red);
-                            //painter.drawRect(topRect);
-                            painter.drawPolygon(topPolygon);
-                            //painter.drawPixmap(QRect(x,y,240,240),third_warning);
+                    // Draw the rescaled backup assistant point
+                    if (assistantRect.contains(assistantXp, assistantYp)) {
+                        if(dist > GREEN_DISTANCE){
+                            painter.setPen(pero);
+                            painter.drawEllipse(QPoint(assistantXp, assistantYp),2,2);
                         }
-                        else if(dist < GREEN_DISTANCE){
-                            QPainter painter(this);
-                            painter.setBrush(Qt::yellow);
-                            //painter.drawRect(topRect);
-                            painter.drawPolygon(topPolygon);
-                            //painter.drawPixmap(QRect(x,y,240,240),second_warning);
+                        else if(dist > YELLOW_DISTANCE){
+                            painter.setPen(yellow_pen);
+                            painter.drawEllipse(QPoint(assistantXp, assistantYp),2,2);
                         }
                         else{
-                            //painter.drawImage(QRect(x,y,50,50),first_warning);
-                            QPainter painter(this);
-                            painter.setBrush(Qt::green);
-                            //painter.drawRect(topRect);
-                            painter.drawPolygon(topPolygon);
-                            //painter.drawPixmap(QRect(x,y,240,240),first_warning);
+                            painter.setPen(red_pen);
+                            painter.drawEllipse(QPoint(assistantXp, assistantYp),2,2);
                         }
-
                     }
-                }
-
-                if(uhol < 135 && uhol > 45){
-
-                    if(dist < YELLOW_DISTANCE){
-                        QPainter painter(this);
-                        painter.setBrush(Qt::red);
-                        //painter.drawRect(leftRect);
-                        painter.drawPolygon(leftPolygon);
-
-                    }
-                    else if(dist < GREEN_DISTANCE){
-                        QPainter painter(this);
-                        painter.setBrush(Qt::yellow);
-                        //painter.drawRect(leftRect);
-                        painter.drawPolygon(leftPolygon);
-
-                    }
-                    else{
-                        QPainter painter(this);
-                        painter.setBrush(Qt::green);
-                        //painter.drawRect(leftRect);
-                        painter.drawPolygon(leftPolygon);
-
-                    }
-                }
-
-                if(uhol < 225 && uhol > 135){
-
-                    if(dist < YELLOW_DISTANCE){
-                        QPainter painter(this);
-                        painter.setBrush(Qt::red);
-                        //painter.drawRect(bottomRect);
-                        painter.drawPolygon(bottomPolygon);
-
-                    }
-                    else if(dist < GREEN_DISTANCE){
-                        QPainter painter(this);
-                        painter.setBrush(Qt::yellow);
-                        //painter.drawRect(bottomRect);
-                        painter.drawPolygon(bottomPolygon);
-                    }
-                    else{
-                        QPainter painter(this);
-                        painter.setBrush(Qt::green);
-                        //painter.drawRect(bottomRect);
-                        painter.drawPolygon(bottomPolygon);
-                    }
-
+                    int xs=assistantRect.width()-(assistantRect.width()/2+2*sin((360.0)*3.14159/180.0))+assistantRect.topLeft().x();
+                    int ys=assistantRect.height()-(assistantRect.height()/2+2*cos((360.0)*3.14159/180.0))+assistantRect.topLeft().y();
+                    painter.drawEllipse(QPoint(xs, ys),20,20);
+                    painter.drawEllipse(QPoint(xs, ys-12),4,4);
 
                 }
-
-
-                if(uhol < 315 && uhol > 225){
-                    if(dist < YELLOW_DISTANCE){
-                        QPainter painter(this);
-                        painter.setBrush(Qt::red);
-                        //painter.drawRect(rightRect);
-                        painter.drawPolygon(rightPolygon);
-                    }
-                    else if(dist < GREEN_DISTANCE){
-                        QPainter painter(this);
-                        painter.setBrush(Qt::yellow);
-                        //painter.drawRect(rightRect);
-                        painter.drawPolygon(rightPolygon);
-                    }
-                    else{
-                        QPainter painter(this);
-                        painter.setBrush(Qt::green);
-                        //painter.drawRect(rightRect);
-                        painter.drawPolygon(rightPolygon);
-                    }
-                }
-
-            }
 
             }
         }
-
-        if(main_window->backup_assistant==true){
-            //main_window->updateLaserPicture=0;
-            // QPainter painter(this);
-            // painter.setBrush(Qt::black); // Set the brush color
-
-            // // Define the rectangle for the frame
-            // QRect rect = this->geometry().adjusted(5, 10, -5, -10);
-            // painter.drawRect(rect);
-
-            // Calculate the size of the backup assistant rectangle (one-third of the width and height of the frame)
-            int assistantWidth = rect.width() / 3;
-            int assistantHeight = rect.height() / 3;
-
-            // Calculate the position of the backup assistant rectangle at the top in the middle of the frame
-            int assistantX = rect.center().x() - assistantWidth / 2;
-            int assistantY = rect.top();
-
-            // Define the backup assistant rectangle
-            QRect assistantRect(assistantX, assistantY, assistantWidth, assistantHeight);
-            // assistantRect.translate(0, rect.top()+50);
-            // Draw the backup assistant rectangle
-            QPainter painter(this);
-            painter.fillRect(assistantRect, Qt::black); // Fill the rectangle with blue color
-
-            // Loop through the laser data and draw backup assistant points within the rectangle
-            for (int k = 0; k < main_window->copyOfLaserData.numberOfScans; k++) {
-                int dist = main_window->copyOfLaserData.Data[k].scanDistance; // Distance divided by 20 (adjust as needed)
-                double angle = 360.0 - main_window->copyOfLaserData.Data[k].scanAngle;
-                int xp = rect.width() - (rect.width() / 2 + dist/5 * 2 * sin(angle * M_PI / 180.0)) + rect.topLeft().x();
-                int yp = rect.height() - (rect.height() / 2 + dist/5 * 2 * cos(angle * M_PI / 180.0)) + rect.topLeft().y();
-
-
-                // // Rescale the coordinates to fit within the backup assistant rectangle
-                // int assistantXp = assistantRect.x() + (xp - rect.x()) * assistantRect.width() / rect.width();
-                // int assistantYp = assistantRect.y() + (yp - rect.y()) * assistantRect.height() / rect.height();
-
-                // Calculate the position of the point relative to the center of the frame
-                // int relativeX = xp - rect.center().x();
-                // int relativeY = yp - rect.center().y();
-
-                // // Calculate the position of the rescaled point relative to the center of the backup assistant rectangle
-                // int assistantXp = assistantRect.center().x() + relativeX * assistantRect.width() / (2 * rect.width());
-                // int assistantYp = assistantRect.center().y() + relativeY * assistantRect.height() / (2 * rect.height());
-
-                // Rescale the coordinates to fit within the assistant frame
-                int assistantXp = assistantRect.x() + (xp - rect.x()) * assistantRect.width() / rect.width();
-                int assistantYp = assistantRect.y() + (yp - rect.y()) * assistantRect.height() / rect.height();
-
-                // Draw the rescaled backup assistant point
-                if (assistantRect.contains(assistantXp, assistantYp)) {
-                    if(dist > GREEN_DISTANCE){
-                        painter.setPen(pero);
-                        painter.drawEllipse(QPoint(assistantXp, assistantYp),2,2);
-                    }
-                    else if(dist > YELLOW_DISTANCE){
-                        painter.setPen(yellow_pen);
-                        painter.drawEllipse(QPoint(assistantXp, assistantYp),2,2);
-                    }
-                    else{
-                        painter.setPen(red_pen);
-                        painter.drawEllipse(QPoint(assistantXp, assistantYp),2,2);
-                    }
-                }
-                int xs=assistantRect.width()-(assistantRect.width()/2+2*sin((360.0)*3.14159/180.0))+assistantRect.topLeft().x();
-                int ys=assistantRect.height()-(assistantRect.height()/2+2*cos((360.0)*3.14159/180.0))+assistantRect.topLeft().y();
-                painter.drawEllipse(QPoint(xs, ys),20,20);
-                painter.drawEllipse(QPoint(xs, ys-12),4,4);
-
-            }
-
-        }
+    }
 
 }
 
 
+}
